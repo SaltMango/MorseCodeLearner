e29d6e84441d5d09ba6a8e085363e2414acde6dc
saltmango <7733Nath>
2019-05-30 19:42:50 +0530

    1st day

--- app/src/main/java/com/github/saltmango/morse/MainActivity.kt
+++ app/src/main/java/com/github/saltmango/morse/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.github.saltmango.morse
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +20,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        button_learn.setOnClickListener(){
+            Toast.makeText(applicationContext,"click",Toast.LENGTH_LONG).show()
+           // Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,Main2Activity::class.java)
+            startActivity(intent)
+
+        }
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
+
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
